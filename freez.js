29d17a6a3fa59fb4d309624712010659981c73cb
fazleh-rakib <rakibfazleh@gmail.com>
2023-11-27 20:52:13 +0600

    freez

--- freez.js
+++ freez.js
@@ -9,7 +9,7 @@ const key = Object.keys(bottle)
 console.log(key);
 const valus = Object.values(bottle)
 console.log(valus);
-
+Object.freeze(bottle)
 bottle.price = 100   
 bottle.height = 10  
 console.log(bottle);   
